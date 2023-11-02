86fab68778cee642ca39718e9a5695c5f5383fa6
wangshei <wangshei@usc.edu>
2023-11-02 16:57:36 -0700

    sort category done! all normalizations are correct

--- search_result/sortcategory.php
+++ search_result/sortcategory.php
@@ -12,39 +12,53 @@ if($mysql->connect_errno){
     echo "Database Connection Error:". $mysql->connect_errno;
     exit();
 }
-$delete = "DELETE FROM sub_category";
-$deleteresults = $mysql->query($delete);
+//$delete = "DELETE FROM sub_category";
+//$deleteresults = $mysql->query($delete);
 
 //create querey that pulls from database
-$sql = "SELECT paper_x_category_id, category FROM paper_x_category ";
+$sql = "SELECT paper_x_category_id, category, sub_category_id FROM paper_x_category ";
 $sub_category_id = 0;
 
 //save the query into a variable
 $results = $mysql->query($sql);
     while ($currentrow = $results->fetch_assoc())  {
         #sql commands that will be used each time results are fetched
-        $checksql = "SELECT sub_category FROM sub_category "; #check if the current sub_category exists already
-        $insertsql = "INSERT INTO sub_category (sub_category_id, sub_category, primary_category_id) VALUES ";
-        $updatesql = "UPDATE paper_x_category SET sub_category_id = ";
+        $checksql = "SELECT sub_category_id, sub_category FROM sub_category ";
+        #check if the current sub_category exists already, WHERE clause coming up later in the code to define what the category is
+        $insertsql = "INSERT INTO sub_category (sub_category_id, sub_category, prim_category_id) VALUES ";
+        #put the info of this sub category in to the sub_category table, values yet to be specified
+        $updatesql = "UPDATE paper_x_category SET sub_category_id = "; #update the sub_category_id in the paper_x_vategory table
 
-        $current = $currentrow['category'];
-        echo "Current is ". $current;
+        $current = $currentrow['category']; #give a new name for category from paper_x_category table
+        echo "<br><br>Current is ". $current;
+        $currentid = $currentrow["sub_category_id"];
+        echo "currentid is ".$currentid;
         $checksql = $checksql."WHERE sub_category = '". $current."'";
+        $paper_x_category_id = $currentrow['paper_x_category_id'];
+        echo "paper_x_category_id is".$paper_x_category_id;
+
         echo "<br> Checksql is ".$checksql;
-        $checkresults = $mysql->query($checksql);
+        $checkresults = $mysql->query($checksql); #run checksql query
         var_dump($checkresults);
         $rows = $checkresults->num_rows;
-        echo "there are". $rows. "rows";
+        echo "there are ". $rows. " rows";
+
+
 
         if (!$rows) {
 
             $sub_category_id +=1;
-            echo "no category found, starting to add" . $current . " to sub category";
-            $insertsql = $insertsql . "(".$sub_category_id.",'" . $current."'";
-            $updatesql = $updatesql.$sub_category_id." WHERE paper_x_category_id =".$currentrow['paper_x_category_id'];
+
+            $updatesql = $updatesql.$sub_category_id." WHERE paper_x_category_id =".$paper_x_category_id;
             echo "<br> updatesql".$updatesql. "and sub_category_id is".$sub_category_id;
             $updateresults = $mysql->query($updatesql);
-            if (strpos($current, "q-bio") !== false) {
+
+            echo "no category found, starting to add" . $current . " to sub category";
+            $insertsql = $insertsql . "(".$sub_category_id.",'" . $current."'"; # add values for sub_category_id and sub_category, needing to check category
+
+            if (strpos($current, "CoRR") !== false) {
+                $insertsql = $insertsql . ", 3)";
+            }else if (strpos($current, "q-bio") !== false) {
                 $insertsql = $insertsql . ", 4)";
             }else if (strpos($current, "q-fin") !== false) {
                 $insertsql = $insertsql . ", 5)";
@@ -59,16 +73,26 @@ $results = $mysql->query($sql);
             }else {
                 $insertsql = $insertsql . ", 1)";
             }
-            echo "<br> insertsql is ".$insertsql;
+            echo "<br> running insertsql ".$insertsql;
             $insertresult = $mysql->query($insertsql);
         }
         else {
+            echo ("there are more than 0 rows, meaning this sub category is already recorded");
 
-            $updatesql = $updatesql."$sub_category_id WHERE paper_x_category_id =".$currentrow['paper_x_category_id'];
-            echo "<br> the else Update sql is ". $updatesql;
+            $checkinfo = $checkresults -> fetch_assoc();
+            var_dump($checkinfo["sub_category_id"]);
+            echo "<strong>updating sub_category_id now!</strong>";
+            $updatesql = $updatesql.$checkinfo["sub_category_id"]." WHERE paper_x_category_id =".$paper_x_category_id;
+            echo "updatesql is ".$updatesql;
             $updateresults = $mysql->query($updatesql);
+//            var_dump($checkinfo["sub_category_id"]);
+//            $updatesql = $updatesql.$checkinfo["sub_category_id"]." WHERE paper_x_category_id =".$paper_x_category_id;
+//            echo "<br> the else Update sql is ". $updatesql;
+//            $updateresults = $mysql->query($updatesql);
+
         }
 
     }
+
 
 ?>
