f58ff45c2d40cc2a59212574e904b1b678e4e425
Calix Huang <calix.huang1@gmail.com>
2023-11-07 16:14:47 -0800

    styling improvements

--- search_result/login.php
+++ search_result/login.php
@@ -1,12 +1,12 @@
 <html lang = "en">
 <head>
-    <title>Register</title>
+    <title>Sign In</title>
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body>
 
 <div className="w-full" style="background-image: url('assets/background.svg'); background-repeat: no-repeat; background-size: cover;">
-<div class="absolute top-0 w-full py-4 px-3" style="background: linear-gradient(to top, rgba(255,255,255,0) 0%, rgba(255,255,255,0.8) 50%, rgba(255,255,255,0.8) 100%);">
+  <div class="absolute top-0 w-full py-4 px-3" style="background: linear-gradient(to top, rgba(255,255,255,0) 0%, rgba(255,255,255,0.8) 50%, rgba(255,255,255,0.8) 100%);">
         <div class="max-w-6xl mx-auto flex flex-row items-center justify-between">
             <a href="search.php">
                 <img src="assets/logo.svg" class="w-32" />
@@ -25,11 +25,7 @@
     <div class="mt-32 py-8 bg-white">
         <div class="max-w-lg mx-auto">
             <form class="border border-gray-200 rounded-2xl shadow-sm p-8 space-y-4">
-              <h1 class="text-2xl font-semibold">Register</h1>
-              <div class="space-y-2">
-                <label class="block">Email</label>
-                <input type="email" placeholder="Type here" class="w-full py-2 px-4 rounded-lg border border-gray-200 shadow-sm" />
-              </div>
+              <h1 class="text-2xl font-semibold">Login</h1>
               <div class="space-y-2">
                 <label class="block">Username</label>
                 <input type="text" placeholder="Type here" class="w-full py-2 px-4 rounded-lg border border-gray-200 shadow-sm" />
@@ -38,14 +34,10 @@
                 <label class="block">Password</label>
                 <input type="password" placeholder="**********" class="w-full py-2 px-4 rounded-lg border border-gray-200 shadow-sm" />
               </div>
-              <div class="space-y-2">
-                <label class="block">Confirm Password</label>
-                <input type="password" placeholder="**********" class="w-full py-2 px-4 rounded-lg border border-gray-200 shadow-sm" />
-              </div>
               <div class="">
-                <button class="mt-4 text-white w-full py-2 px-3 rounded-full bg-blue-400 border-none hover:bg-blue-500 cursor-pointer">Register</button>
+                <button class="mt-4 text-white w-full py-2 px-3 rounded-full bg-blue-400 border-none hover:bg-blue-500 cursor-pointer">Login</button>
               </div>
-              <p class="text-sm text-center text-gray-500">Already have an account? <a href="login.php" class="text-blue-400 underline cursor-pointer hover:text-blue-500 focus:outline-none font-medium">Login here</a></p>
+              <p class="text-sm text-center text-gray-500">New to Intuitive Paper? <a href="register.php" class="text-blue-400 underline cursor-pointer hover:text-blue-500 focus:outline-none font-medium">Register now</a></p>
             </form>
         </div>
     </div>
--- search_result/search.php
+++ search_result/search.php
@@ -25,23 +25,52 @@ $sql = "SELECT categories FROM xie_import_6000 ";
 $results = $mysql->query($sql);
 ?>
 
-<div className="w-full">
+<div class="w-full">
+    <div class="absolute top-0 w-full py-4 px-3" style="background: linear-gradient(to top, rgba(255,255,255,0) 0%, rgba(255,255,255,0.8) 50%, rgba(255,255,255,0.8) 100%);">
+        <div class="max-w-6xl mx-auto flex flex-row items-center justify-between">
+            <a href="search.php">
+                <img src="assets/logo.svg" class="w-32" />
+            </a>
+            <div class="flex flex-row items-center space-x-2">
+                <a href="login.php">
+                    <button class="py-2 px-4 rounded-full border border-gray-200 transition hover:bg-gray-100 bg-white text-sm text-gray-700">Login</button>
+                </a>
+                <a href="register.php">
+                    <button style="border-color: #2D1F63; background-color: #2D1F63;" class="py-2 px-4 rounded-full border text-sm text-white">Register</button>
+                </a>
+            </div>
+        </div>
+    </div>
+
+
+
     <img src="assets/background.svg" class="absolute left-0 top-0 w-full" style="z-index: -1; min-height: 300px;" />
     <div class="mt-52 py-8 px-4 h-full" style="background: linear-gradient(to bottom, rgba(255,255,255,0) 0%, rgba(255,255,255,1) 20%, rgba(255,255,255,1) 100%);">
         <div class="max-w-2xl mx-auto">
             <div class="text-center">
                 <h1 class="text-5xl font-extrabold" style="color: #2D1F63;">Intuitive Paper</h1>
                 <p class="mt-3 text-lg font-medium text-gray-500">Find the best research on ArXiv</p>
-                <form action="results.php" method="GET" class="mt-8 flex flex-row items-center space-x-4">
-                    <input name="search_title" class="w-full shadow-sm py-4 px-7 rounded-full border border-gray-200 focus:outline-none text-lg bg-white" type="text" placeholder="Search paper topics" />
-                    <input type="hidden" name="author" value="" placeholder="Author">
-                    <input type="hidden" name="doi" value="" placeholder="Search by doi">
-                    <input type="hidden" name="categories" value="ALL" placeholder="Search by doi">
-                    <button style="background-color: #2D1F63;" class="rounded-full p-4 shadow-sm border-none focus:outline-none">
-                        <img src="assets/search.svg" class="w-8" />
-                    </button>
+                <form action="results.php" method="GET" class="space-y-3">
+                    <div class="mt-8 flex flex-row items-center space-x-4">
+                        <input name="search_title" class="w-full shadow-sm py-4 px-7 rounded-full border border-gray-200 focus:outline-none text-lg bg-white" type="text" placeholder="Search paper topics" />
+                        <input type="hidden" name="author" value="" placeholder="Author">
+                        <input type="hidden" name="doi" value="" placeholder="Search by doi">
+                        <input type="hidden" name="categories" value="ALL" placeholder="Search by doi">
+                        <button style="background-color: #2D1F63;" class="rounded-full p-4 shadow-sm border-none focus:outline-none">
+                            <img src="assets/search.svg" class="w-8" />
+                        </button>
+                    </div>
+                    <div class="max-w-xs mx-auto">
+                        <label for="location" class="mr-1 text-sm font-medium leading-6 text-gray-500">Filter by</label>
+                        <select id="location" name="location" style="width: 125px" class="text-sm mt-1 mx-auto w-full rounded-full border-0 py-1 pl-2 pr-5 text-gray-900 ring-1 ring-inset ring-gray-200 sm:text-sm sm:leading-6">
+                            <option selected>Title</option>
+                            <option>Author</option>
+                            <option>DOI</option>
+                            <option>Category</option>
+                        </select>
+                        </div>
+                    </div>
                 </form>
-            </div>
             <div class="mt-16 text-center">
                 <h5 class="text-lg text-gray-500">Try searching</h5>
                 <div class="mt-3 flex flex-row items-center justify-center flex-wrap">
