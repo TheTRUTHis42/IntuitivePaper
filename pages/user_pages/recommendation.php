61b74ead7b5e3a3abd87dec4b1c827e1223c8df4
wangshei <wangshei@usc.edu>
2023-12-04 00:13:50 -0800

    visualization of data on recommendations page

--- pages/user_pages/recommendation.php
+++ pages/user_pages/recommendation.php
@@ -2,12 +2,33 @@
 session_start();
 ?>
 <html>
+    <head>
+        <style>
+            #container {
+                padding: 30px;
+                background-color: olive;
+                text-align: left;
+                color:white;
+            }
+
+            .box {
+                float:left;
+                text-align:right;
+                height: 20px;
+                width: 200px;
+                margin: 5px;
+            }
+            .bar {
+                background-color: red; min-width: 30px;
+            }
+        </style>
+    </head>
     <body>
-    <a href="../search.php">
-        <img src="../assets/logo.svg"/><br><br>
-    </a>
-    </body>
-</html>
+        <a href="../search.php">
+            <img src="../assets/logo.svg"/><br><br>
+        </a>
+
+
 <?php
 
 // Database connection
@@ -35,11 +56,11 @@ if (isset($_SESSION['userLoggedIn'], $_SESSION['userId'], $_GET['id']) && $_SESS
     echo "<a href = 'browsing_history.php'>Browsing History <a/>";
     echo "<a href = 'recommendation.php'>Recommendation Management <a/>";
 
-    echo"<h1>Recommendation Management</h1>";
-    while ($currentrow = $result -> fetch_assoc()) {
+    echo "<h1>Recommendation Management</h1>";
+    while ($currentrow = $result->fetch_assoc()) {
         echo $currentrow['sub_categories_name'];
-        echo "<form action = 'delete_category.php'> <input type='hidden' name = 'id' value = ". $currentrow['user_x_category_id'] . ">";
-        echo "<input type='submit'value = 'Delete'>"."<br>";
+        echo "<form action = 'delete_category.php'> <input type='hidden' name = 'id' value = " . $currentrow['user_x_category_id'] . ">";
+        echo "<input type='submit'value = 'Delete'>" . "<br>";
     }
 
     echo "</form><form action = 'update_category.php'><h3>Add Category</h3>";
@@ -47,12 +68,32 @@ if (isset($_SESSION['userLoggedIn'], $_SESSION['userId'], $_GET['id']) && $_SESS
     //save the query into a variable
     $results = $mysql->query($sql);
     echo "<select name ='category''> ";
-    while ($currentrow = $results->fetch_assoc()){
+    while ($currentrow = $results->fetch_assoc()) {
         echo "<option name = 'category' value ='" . $currentrow['sub_categories_id'] . "'>" .
             $currentrow['sub_categories_name'] . "</option>";
     }
     echo "</select><input type='submit' ></form>";
+
+    $sql1 = "SELECT COUNT(*) AS totalsubcategory FROM sub_categories";
+
+    $results1 = $mysql -> query($sql1);
+
+    $sql2 = "SELECT COUNT(*) AS totalcategory, prim_category_id
+         FROM sub_categories
+         GROUP BY prim_category_id";
+    $results2 = $mysql -> query($sql2);
+
+    $currentrow1 = $results1->fetch_assoc();
+    echo "<br><br>Totals visualized...<strong>".$currentrow1["totalsubcategory"]."</strong><br>";
+    $results2 = $mysql -> query($sql2);
+    while($currentrow = $results2 ->fetch_assoc()){
+        echo "<div class='box'>".$currentrow["prim_category_id"].":</div>";
+
+        echo "    <div class='box bar' style='width:".(floatval($currentrow["totalcategory"])/2)."px'>&nbsp;</div>";
+        echo $currentrow["totalcategory"]."<br style='clear:both'>";
+    }
 }
+
 
 ?>
 
